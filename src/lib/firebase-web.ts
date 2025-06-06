164fcad3ed585957eb05e66e256057a1d0112bac
Afrie Irham <afrieirham.work@gmail.com>
2025-06-06 14:56:13 +0800

    setup t3 app

--- src/lib/firebase-web.ts
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from "firebase/app";
-import { getStorage } from "firebase/storage";
-
-const app = initializeApp({
-  storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}.appspot.com`,
-});
-
-export const storage = getStorage(app);
